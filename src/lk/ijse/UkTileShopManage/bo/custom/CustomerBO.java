9372ccc62a2f1d39c76f108d3d0813fb2b327808
dilushaeka <dilushaeka99@gmail.com>
2024-01-08 23:08:57 +0530

    Added add customer bo

---
+++ src/lk/ijse/UkTileShopManage/bo/custom/CustomerBO.java
@@ -0,0 +1,23 @@
+package lk.ijse.UkTileShopManage.bo.custom;
+
+import bo.SuperBO;
+import dto.CustomerDTO;
+import javafx.collections.ObservableList;
+
+import java.sql.SQLException;
+
+public interface CustomerBO extends SuperBO {
+    boolean addCustomer(CustomerDTO customer) throws ClassNotFoundException, SQLException;
+
+    boolean deleteCustomer(String id) throws ClassNotFoundException, SQLException;
+
+    boolean updateCustomer(CustomerDTO id) throws ClassNotFoundException, SQLException;
+
+    CustomerDTO searchCustomer(String id) throws ClassNotFoundException, SQLException;
+
+    ObservableList<CustomerDTO> getAllCustomer() throws ClassNotFoundException, SQLException;
+
+    int getRowCount()throws ClassNotFoundException,SQLException;
+
+}
+
